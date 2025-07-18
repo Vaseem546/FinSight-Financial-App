35f83fe7e65f8a293c0960bd51a6aa7b150c9b7f
Vaseem Basha Syed <168512400+Vaseem546@users.noreply.github.com>
2025-07-18 19:28:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -327,4 +327,6 @@ def remove_from_portfolio():
 # ---------------- RUN ---------------- #
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Railway provides the port
+    app.run(debug=False, host='0.0.0.0', port=port)
+
